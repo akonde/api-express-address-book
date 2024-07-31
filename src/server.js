798658985e858bfaf200e6ab521ee0c6f921ae4c
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-07-31 14:03:02 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,13 +1,52 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const contacts = require("../data/contacts");
+const app = express();
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 // write your app code here
 
+// get all contacts
+app.get("/contacts", function (req, res) {
+  res.status(200).json({
+    contacts,
+  });
+});
 
-module.exports = app
+// add  a new contact
+app.post("/contacts", function (req, res) {
+  const { firstName, lastName, street, city, type, email, linkedin, twitter } =
+    req.body;
+  if (
+    !(firstName || !lastName || !street || !city || !type ||  !email || !linkedin || !twitter)
+  ) {
+    res.json({
+      message: "all are required!",
+    });
+  }
+
+  const newContact = {
+    id: contacts.length + 1,
+    firstName,
+    lastName,
+    street,
+    city,
+    type,
+    email,
+    linkedin,
+    twitter,
+  };
+
+  contacts.push(newContact);
+
+  res.status(201).json({
+    message: "New Contact Added!",
+    contact: newContact,
+  });
+});
+
+module.exports = app;
