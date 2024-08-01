eba1fdecf55cab0ba747c48b0570e30ce4e5aafb
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-08-01 01:22:42 +0100

    updated

--- src/server.js
+++ src/server.js
@@ -22,7 +22,16 @@ app.post("/contacts", function (req, res) {
   const { firstName, lastName, street, city, type, email, linkedin, twitter } =
     req.body;
   if (
-    !(firstName || !lastName || !street || !city || !type ||  !email || !linkedin || !twitter)
+    !(
+      firstName ||
+      !lastName ||
+      !street ||
+      !city ||
+      !type ||
+      !email ||
+      !linkedin ||
+      twitter
+    )
   ) {
     res.json({
       message: "all are required!",
@@ -47,6 +56,71 @@ app.post("/contacts", function (req, res) {
     message: "New Contact Added!",
     contact: newContact,
   });
+});
+
+// Get conatct by ID
+app.get("/contacts/:id", function (req, res) {
+  const id = parseInt(req.params.id, 10);
+
+  const contact = contacts.find(function (contact) {
+    return contact.id === id;
+  });
+
+  res.status(200).json({
+    contact,
+  });
+});
+
+// Delete contact by ID
+app.delete("/contacts/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+
+  // delete user from array
+  contacts = contacts.filter(function (user) {
+    return contacts.id !== id
+  })
+
+
+  res.status(200).json({
+    message: `Deleted user ${id} successfully`,
+    contacts
+  })
+
+})
+
+// update contact by ID
+app.put("/contacts/:id", function (req,  res) {
+  const id = (req.params.id,  10)
+  const { firstName, lastName, street, city, type, email, linkedin, twitter } = req.body;
+
+})
+
+if (!firstName || !lastName || !street || !city || !type || !email || !linkedin || !twitter) {
+  return res.status(400).json({
+    message: "All fields are required!",
+  });
+}
+
+
+const updatedContact = {
+  id,
+  firstName,
+  lastName,
+  street,
+  city,
+  type,
+  email,
+  linkedin,
+  twitter,
+};
+
+contacts[contactIndex] = updatedContact;
+const contactIndex = contacts.find(function (contact) {
+  return contact.id === id;
+});
+res.status(200).json({
+  message: "Contact Updated!",
+  contact: updatedContact,
 });
 
 module.exports = app;
